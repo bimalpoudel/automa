6d47a0f38e987333103945eca86db631218fbaba
bimal.poudel <bimal.poudel@3d55a656-fcd0-140a-4b87-3ef75621f877>
2011-04-07 07:08:44 +0000

    User groups sorted.

--- trunk/sort-users.sh
+++ trunk/sort-users.sh
@@ -5,10 +5,17 @@
 # Usage
 #   /bin/bash sort-users.sh
 
-cat /etc/passwd|sort > /tmp/passwd
+# sort passwd file
+cat /etc/passwd | sort > /tmp/passwd
 cat /tmp/passwd > /etc/passwd
 rm -f /tmp/passwd
 
+# sort shadow file
 cat /etc/shadow | sort > /tmp/shadow
 cat /tmp/shadow > /etc/shadow
 rm -f /tmp/shadow
+
+# sort group file
+cat /etc/group | sort > /tmp/group
+cat /tmp/group > /etc/group
+rm -f /tmp/group
