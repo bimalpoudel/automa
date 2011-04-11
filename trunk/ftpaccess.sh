ff493782c1803fc3a7b9c18e00d9762fe92669fd
bimal.poudel <bimal.poudel@3d55a656-fcd0-140a-4b87-3ef75621f877>
2011-04-11 11:06:59 +0000

    Disallow some users to access SSH but FTP only.

---
+++ trunk/ftpaccess.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Disallows users to login via SSH
+echo "This accout is for ftp access only" > /bin/ftpaccess
+echo "exit 0" >> /bin/ftpaccess
+chmod +x /bin/ftpaccess
+
+# Now, modify the default shell of the user to /bin/ftpaccess
+# Notes: Read somewhere about this technique, but it does not
+# work in Ubuntu.
