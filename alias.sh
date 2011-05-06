45b60f629ead5b6659ac48825ade7b69742281c7
bimal.poudel <bimal.poudel@3d55a656-fcd0-140a-4b87-3ef75621f877>
2011-05-06 20:52:55 +0000

    alias to some xampp stack applications added.

---
+++ alias.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Installs various links to standard applications
+# These are different from run time aliases
+# These changes are permanent and allow direct access
+
+# from the XAMPP stack
+ln -s /opt/lampp/bin/php /usr/bin/php
+ln -s /opt/lampp/bin/mysql /usr/bin/mysql
+ln -s /opt/lampp/bin/mysqldump /usr/bin/mysqldump
+ln -s /opt/lampp/lampp /usr/bin/mysqldump
+ln -s /opt/lampp/htpasswd /usr/bin/htpasswd
