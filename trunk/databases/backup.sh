e7ac67010392a660feeade65d0a92b69aeec3e92
bimal.poudel <bimal.poudel@3d55a656-fcd0-140a-4b87-3ef75621f877>
2011-05-06 20:46:18 +0000

    cron script to backup databases once a day.

--- trunk/databases/backup.sh
+++ trunk/databases/backup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # alias: /usr/bin/backup
 # Recurse and backup all possible databases directly from the data directories
+# Change correct username and password
+# Indented to run for automated backups via cron scripts
+#
+# usage
+# /user/bin/backup
 
 MYSQLDUMP="/opt/lampp/bin/mysqldump -uusername -ppassword";
 DUMPTO="/home/database-dumps/all";
@@ -9,6 +14,9 @@ DATADIR="/opt/lampp/var/mysql";
 # Just be sure that the dump location exists
 mkdir -p "${DUMPTO}";
 
+# loop through the data directories
+# and take individual backups
+# assume each directory name as a database name
 for DATABASE in `ls ${DATADIR}`
 do
   if [ -d "${DATADIR}/${DATABASE}" ]; then
